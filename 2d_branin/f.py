a31f4fab0f8143fd177e901516b4442d13528e83
Justin Beland <justin.beland@mail.utoronto.ca>
2017-12-29 19:21:58 -0500

    completed second example

--- 2d_branin/f.py
+++ 2d_branin/f.py
@@ -1,7 +1,7 @@
 import numpy as np
 import math
 
-def branin(X, args=()):
+def f(X, args=()):
 	x = X[:, 0]
 	y = X[:, 1]
 	return np.square(y - (5.1/(4*np.square(math.pi)))*np.square(x) + (5/math.pi)*x - 6) + 10*(1-(1./(8*math.pi)))*np.cos(x) + 10
